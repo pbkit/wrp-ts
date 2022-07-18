a98720fbf8e0e8663fc3fd4f2a251fcd838835c1
Yongwook Choi <hyperflow@kakao.com>
2022-07-18 13:24:46 +0900

    Add channel atom

--- src/jotai/index.ts
+++ src/jotai/index.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 import type { RpcClientImpl } from "https://deno.land/x/pbkit@v0.0.45/core/runtime/rpc.ts";
 import { Socket } from "../socket.ts";
 import { createIosSocket } from "../glue/ios.ts";
-import { createWrpChannel } from "../channel.ts";
+import { createWrpChannel, WrpChannel } from "../channel.ts";
 import { createWrpClientImpl } from "../rpc/client.ts";
 import { createWrpGuest, WrpGuest } from "../guest.ts";
 import { createAndroidSocket } from "../glue/android.ts";
@@ -16,10 +16,16 @@ export const socket = atom<Promise<Socket | null>>(async () => {
   ]).catch(() => null);
 });
 
-export const guest = atom<Promise<WrpGuest | null>>(async (get) => {
+export const channel = atom<WrpChannel | null>((get) => {
   const _socket = get(socket);
   if (!_socket) return null;
-  return await createWrpGuest({ channel: createWrpChannel(_socket) });
+  return createWrpChannel(_socket);
+});
+
+export const guest = atom<Promise<WrpGuest | null>>(async (get) => {
+  const _channel = get(channel);
+  if (!_channel) return null;
+  return await createWrpGuest({ channel: _channel });
 });
 
 export const client = atom<RpcClientImpl | null>((get) => {
